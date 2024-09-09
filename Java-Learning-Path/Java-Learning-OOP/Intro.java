48ef2bfa833520026e1d9f333845de0780eea562
John Mukhwana <Johnbradill67@gmail.com>
2024-09-09 23:27:10 +0300

    First Java updated the name

--- Java-Learning-Path/Java-Learning-OOP/Intro.java
+++ Java-Learning-Path/Java-Learning-OOP/Intro.java
@@ -1,3 +1,7 @@
+
+//Cooment on this code
+//This is a simple java program that prints "Hello World" to the console
+
 public class Intro{
   public static void main(String[] args) {
     System.out.println("Hello World");
