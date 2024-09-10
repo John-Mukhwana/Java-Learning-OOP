8379aee633e74f61ce40c0032b613979ebe9ff91
John Mukhwana <Johnbradill67@gmail.com>
2024-09-10 22:41:20 +0300

    added comments md

---
+++ Java-Learning-Path/Java-Learning-OOP/Comment/JavaComment.java
@@ -0,0 +1,16 @@
+package Comment;
+
+//Comments in java are used to explain the code and make it more readable. They are ignored by the compiler and are not executed. There are two types of comments in java: single line comments and multi-line comments. Single line comments start with // and end at the end of the line. Multi-line comments start with /* and end with */.
+//The code snippet in the file coment.java is an example of a single line comment. The comment // Path: JavaComment/coment.java is a single line comment that explains the path of the file. It is used to provide information about the file and is ignored by the compiler.
+//example of java code to display comments
+
+// Single line comment
+// This is a single line comment
+// It starts with // and ends at the end of the line
+
+public class JavaComment{
+    public static void main(String[] args) {
+      System.out.println("this are the comments starting with //");
+    }
+  }
+  
