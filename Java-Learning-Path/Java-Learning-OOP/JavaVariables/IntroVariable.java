b93a13b99c6c715fef70f181d0e06c382acc0f17
John Mukhwana <Johnbradill67@gmail.com>
2024-09-12 23:52:17 +0300

    worked with variables

---
+++ Java-Learning-Path/Java-Learning-OOP/JavaVariables/IntroVariable.java
@@ -0,0 +1,22 @@
+package JavaVariables;
+
+public class IntroVariable{
+    public static void main(String args[]){
+        int weight=10;
+        String name="John";
+        double salary=1200.50;
+        float height=5;
+        boolean maritalStatus=true;
+
+        if (maritalStatus)
+            {
+                System.out.println("Name: "+name);
+                System.out.println("Salary: "+salary);
+                System.out.println("Height: "+height);
+                System.out.println("Marital Status: Married");
+                System.out.println("Weight: "+weight);
+            }
+            
+
+    }
+}
