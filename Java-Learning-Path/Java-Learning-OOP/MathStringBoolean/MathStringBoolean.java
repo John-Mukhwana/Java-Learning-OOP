01d8fdcf48edd68991e63fad59958b284c258701
John Mukhwana <Johnbradill67@gmail.com>
2024-09-19 19:38:16 +0300

    added the boolean code

--- Java-Learning-Path/Java-Learning-OOP/MathStringBoolean/MathStringBoolean.java
+++ Java-Learning-Path/Java-Learning-OOP/MathStringBoolean/MathStringBoolean.java
@@ -26,3 +26,12 @@ package MathStringBoolean;
 //         System.out.println(Math.sqrt(x));
 //     }
 // }
+
+
+public class MathStringBoolean {
+  public static void main(String[] args) {
+    int myAge = 25;
+    int votingAge = 18;
+    System.out.println(myAge >= votingAge); // returns true (25 year olds are allowed to vote!) 
+  }
+}
