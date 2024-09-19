9f8d330f7c8817f07d67e08b383d2c965aeb7f4f
John Mukhwana <Johnbradill67@gmail.com>
2024-09-19 19:04:36 +0300

    implemented the code to count number of strings

---
+++ Java-Learning-Path/Java-Learning-OOP/MathStringBoolean/MathStringBoolean.java
@@ -0,0 +1,10 @@
+
+package MathStringBoolean;
+
+// public class MathStringBoolean{
+//     public static void main(String[] args){
+//         String txt="asdfghvngthejnbrtygjr";
+//         System.out.println("the length of string is:" +txt.length());
+//     }
+// }
+
