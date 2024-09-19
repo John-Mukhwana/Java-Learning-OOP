4d55597a9fcf843cf7a34efab2ba2913a63610b9
John Mukhwana <Johnbradill67@gmail.com>
2024-09-19 19:21:45 +0300

    implemented the code to work out on math

--- Java-Learning-Path/Java-Learning-OOP/MathStringBoolean/MathStringBoolean.java
+++ Java-Learning-Path/Java-Learning-OOP/MathStringBoolean/MathStringBoolean.java
@@ -8,3 +8,21 @@ package MathStringBoolean;
 //     }
 // }
 
+
+// public class MathStringBoolean{
+
+//     public static void main(String[] args){
+//         int x=57;
+//         int y=83;
+//         System.out.println(Math.max(x,y));
+//     }
+// }
+
+public class MathStringBoolean{
+
+    public static void main(String[] args){
+        int x=57;
+        
+        System.out.println(Math.sqrt(x));
+    }
+}
