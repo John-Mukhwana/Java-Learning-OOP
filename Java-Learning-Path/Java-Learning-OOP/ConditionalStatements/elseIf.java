2ececb31927bcfad7f56a1a8f6fdd127b3196ada
John Mukhwana <Johnbradill67@gmail.com>
2024-09-24 15:34:27 +0300

    Added the elseIf file

--- Java-Learning-Path/Java-Learning-OOP/ConditionalStatements/elseIf.java
+++ Java-Learning-Path/Java-Learning-OOP/ConditionalStatements/elseIf.java
@@ -1,15 +1,17 @@
-
-
 package ConditionalStatements;
 
-public class elseIf{
+public class elseIf {
     public static void main(String[] args){
-        int age = 2;
-        if(age>18){
+        int age = 19;
+        if(age > 18){
             System.out.println("You are an adult");
         }
-        else{
+        else if(age < 18){
             System.out.println("You are not an adult");
         }
+        else{
+            System.out.println("You are 18 years old");
+        }
     }
+    
 }
---
+++ Java-Learning-Path/Java-Learning-OOP/ConditionalStatements/ifElse.java
@@ -0,0 +1,15 @@
+
+
+package ConditionalStatements;
+
+public class ifElse{
+    public static void main(String[] args){
+        int age = 2;
+        if(age>18){
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are not an adult");
+        }
+    }
+}
