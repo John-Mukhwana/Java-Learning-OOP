454e3bd6609fdef60c329bb0e812bc7033879f02
John Mukhwana <Johnbradill67@gmail.com>
2024-09-24 15:31:20 +0300

    added the useState to store the entered Values

---
+++ Java-Learning-Path/Java-Learning-OOP/ConditionalStatements/elseIf.java
@@ -0,0 +1,15 @@
+
+
+package ConditionalStatements;
+
+public class elseIf{
+    public static void main(String[] args){
+        int age = 2;
+        if(age>18){
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are not an adult");
+        }
+    }
+}
