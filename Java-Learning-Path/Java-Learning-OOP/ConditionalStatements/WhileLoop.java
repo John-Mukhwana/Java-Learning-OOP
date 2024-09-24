a5c165524e5ff886009d9b281186efa1e9ae5c65
John Mukhwana <Johnbradill67@gmail.com>
2024-09-24 16:15:19 +0300

    Added Yatzy game

--- Java-Learning-Path/Java-Learning-OOP/ConditionalStatements/WhileLoop.java
+++ Java-Learning-Path/Java-Learning-OOP/ConditionalStatements/WhileLoop.java
@@ -12,13 +12,29 @@ package ConditionalStatements;
 // }
 
 
-public class WhileLoop {
-    public static void main(String[] args){
-        int countDown=3;
-        while(countDown>0){
-            System.out.println(countDown);
-            countDown--;
-        }
-    }
+// public class WhileLoop {
+//     public static void main(String[] args){
+//         int countDown=3;
+//         while(countDown>0){
+//             System.out.println(countDown);
+//             countDown--;
+//         }
+//     }
     
+// }
+
+
+public class WhileLoop{
+    public static void main(String[] args){
+        int dice =1;
+        while(dice<=6){
+            if(dice<6){
+                System.out.println("No Yatzy");
+                
+        }else{
+             System.out.println("Yatzy");
+        }
+          dice=dice+1;
+    }
+}
 }
