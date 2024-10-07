ee330ce3a9448d7dfb6cb2d8e7665a95fb87b761
John Mukhwana <Johnbradill67@gmail.com>
2024-10-07 15:46:19 +0300

    Added a program to find average of ages

--- Java-Learning-Path/Java-Learning-OOP/JavaArray/Main.java
+++ Java-Learning-Path/Java-Learning-OOP/JavaArray/Main.java
@@ -38,11 +38,37 @@ package JavaArray;
 
 
 //Looping Through an Array with For-Each
-public class Main {
-  public static void main(String[] args) {
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-    for (String i : cars) {
-      System.out.println(i);
-    }    
-  }
-}
+// public class Main {
+//   public static void main(String[] args) {
+//     String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+//     for (String i : cars) {
+//       System.out.println(i);
+//     }    
+//   }
+// }
+
+/*Real-Life Example
+To demonstrate a practical example of using arrays, let's create a program that calculates the average of different ages:*/
+
+// public class Main {
+//   public static void main(String[] args) {
+//     // An array storing different ages
+//     int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+
+//     float avg, sum = 0;
+
+//     // Get the length of the array
+//     int length = ages.length;
+
+//     // Loop through the elements of the array
+//     for (int age : ages) {
+//       sum += age;
+//     }
+    
+//     // Calculate the average by dividing the sum by the length
+//     avg = sum / length;
+
+//     // Print the average
+//     System.out.println("The average age is: " + avg);
+//   }
+// }
